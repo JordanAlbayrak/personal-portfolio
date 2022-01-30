5cf970e19d0a0c8d3098c419edda563d74dab134
Jordan Albayrak <1830867@champlaincollege.qc.ca>
2022-01-29 20:33:02 -0500

    Plated around with some styling and animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import styles from '../../styles/Home.module.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import ProjectSection from '../components/ProjectSection'
 import {useEffect, useState} from "react";
+import Head from "next/head";
 
 const client = require('contentful').createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
@@ -29,6 +30,10 @@ const Home = ({dataList}) => {
     }, [])
 
     useEffect(() => {
+        localStorage.setItem('setLightMode', clicked);
+    }, [clicked])
+
+    useEffect(() => {
         if (mounted) {
             // Create a condition that targets viewports at least 768px wide
             const mediaQuery = window.matchMedia(widthThresh_holdForSidebar);
@@ -49,14 +54,19 @@ const Home = ({dataList}) => {
             // Initial check
             handleTabletChange(mediaQuery)
         }
-    }, [mounted])
+    }, [ ])
 
 
     function handleClick() {
         setClicked(!clicked)
+        let r = document.querySelector(':root')
+        r.style.setProperty('--main-bg-color', clicked ? 'darkolivegreen' : 'white')
+        r.style.setProperty('--main-text-color', clicked ? 'white' : 'black')
+        // console.log(r.style.getPropertyValue('--main-bg-color'))
+
+        // localStorage.gay = clicked;
+        // localStorage.setItem('gay', clicked);
     }
-
-
 
     let hamburgerNav;
     if (hamburger){
@@ -65,20 +75,42 @@ const Home = ({dataList}) => {
     else {
         hamburgerNav = null;
     }
+
+
     return(
-        <div className={''}>
+        <div>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                        <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap"
+                              rel="stylesheet"/>
+                <title> Jordan Portfolio</title>
+            </Head>
         {/*<NavBar/>*/}
             {hamburgerNav}
-        <div className="container card" >
-            <h1 className={clicked ? 'gay' : 'gay2'}> Hello</h1>
-            <button onClick={handleClick}> Click Me!</button>
-            <h1 className={styles.title}><a>Hello</a></h1>
-            <div className={'customerGrid '}>
-                    {dataList.map((data) => (
-                        <ProjectSection data={data.fields} key={data.sys.id} />
-                    ))}
+            <div className="container card" >
+                    <h1 className={''}> Hello</h1>
+                    <button onClick={handleClick} className={'light-mode'}> Light Mode</button>
+                    <div style={{textAlign:"center"}}>
+                        <h1 className={styles.title}><a>Hi 🤟</a></h1>
+                        {/* eslint-disable-next-line react/no-unescaped-entities */}
+                        <h2 className={''} style={{textAlign:"center"}}> My name is Jordan Albayrak <br/>and I'm a developer </h2>
+                        <div className={'d-inline-block'}>
+                        <p className={styles.typewriter} style={{display:"inline-block"}} > Welcome to my virtual portfolio</p>
+                        </div>
+                    </div>
+
+
+
+
+                <div className={''} >
+                    <div className={'customerGrid'}>
+                            {dataList.map((data) => (
+                                <ProjectSection data={data.fields} key={data.sys.id} />
+                            ))}
+                    </div>
+                </div>
             </div>
-        </div>
         </div>
     )
 }
